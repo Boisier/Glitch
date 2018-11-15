3992627acba8570faf4847fd9e6ec4454fd24082
Val <valentin@dufois.fr>
2018-11-14 22:05:22 -0500

    Initial Commit

---
+++ glitch/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  glitch
+//
+//  Created by Valentin Dufois on 14/11/2018.
+//  Copyright © 2018 Valentin Dufois. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
